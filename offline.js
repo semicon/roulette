619070e2acd2692b58800f1ba59b8bc187daa923
semicon <30399464+semicon@users.noreply.github.com>
2019-06-19 19:25:30 +0700

    roulette
    roulette

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1560946908,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/wheel-sheet0.png",
+		"images/frame-sheet0.png",
+		"images/mask-sheet0.png",
+		"images/spinbtn-sheet0.png",
+		"images/spinbtn-sheet1.png",
+		"images/bg1-sheet0.png",
+		"images/bg2-sheet0.png",
+		"images/restartbtn-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
